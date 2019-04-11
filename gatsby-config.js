2be04ea8e329e6369406dee0383565afa006dc49
merlin <romain.dary@renault.com>
2019-04-11 14:04:57 +0200

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Romain DARY Website`,
+        short_name: `website`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     {
